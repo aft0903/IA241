96a64480bdeecb3bdac9a00a21509e6519bbc619
aft0903 <ec2-user@ip-172-31-29-152.ec2.internal>
2021-01-27 18:18:35 +0000

    my Lab2.py file.

---
+++ Lab2.py
@@ -0,0 +1,40 @@
+#Lab2
+#Aileen Towner
+#1/27/2021
+
+# 3.1
+my_name = "Tom"
+print( my_name.upper() )
+
+# 3.2
+my_id = 123
+print( my_id )
+
+#3.3
+#123=my_id  
+#can't do this because you 123 isn't a variable its a value
+#my_id=your_id=123
+#can't do this because your id wasn't declared yet also can't have 2 =
+
+#3.4
+my_id_str='123'
+print (my_id_str)
+
+#3.5
+#my_name + my_id
+#can't add int to string , diff variable types
+
+#3.6
+print(my_name + my_id_str)
+#you can add two strings together, same variable type
+
+#3.7
+print(my_name * 3)
+
+#3.8
+print('hello, world. This is my first python string'.split( "." ))
+
+
+#
+
+
