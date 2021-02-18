ec73be8375477a7a9f4b02348d0618e8a539b14e
aft0903 <ec2-user@ip-172-31-29-152.ec2.internal>
2021-02-18 17:34:21 +0000

    lab5 complete

---
+++ Lab5.py
@@ -0,0 +1,37 @@
+#Aileen Towner 
+#2/18/2021
+
+#3.1
+alien_color= 'red'
+
+if alien_color == 'green':
+    print("Player Earns 5 Points")
+    
+#3.2
+alien_color= 'yellow'
+if alien_color=='green':
+    print("Player Earns 5 Points")
+else:
+    print("Player Earns 10 POints")
+
+#3.3
+favorite_fruits = ['mango','starfruit','wax apple']
+if'mango'in favorite_fruits:
+    print('Fantastic Choice! Mangos are tasty!')
+if 'bananas' in favorite_fruits:
+    print('Gross!')
+if 'guava' in favorite_fruits:
+    print ('Tropical Choice!')
+
+#3.4
+age = 34
+
+if age<10:
+    print("This person is a kid")
+elif  age<20 :
+    print("This person is a teenager")
+else:
+    print("This person is an adult")
+    if age > 65:
+        print("This person is also an elder")
+    
